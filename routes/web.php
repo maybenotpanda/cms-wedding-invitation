47015355ca4f1000e7e9052fd8b1266ccb479a72
maybenotpanda <maybenotpanda@gmail.com>
2025-09-10 16:22:24 +0700

    feat-route: add new route invitation, qr, & barcode

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+use App\Http\Controllers\Home;
+use App\Http\Controllers\Invitation;
+use App\Http\Controllers\GenerateBarcode;
+use App\Http\Controllers\GenerateQrCode;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+Route::get('/', [Home::class, 'Index']);
+Route::get('/invitation', [Invitation::class, 'Index']);
+Route::post('/invitation', [Invitation::class, 'store'])->name('invitation.store');
+
+Route::get('/qr-maps', [GenerateQrCode::class, 'index']);
+Route::get('/qr-maps/download', [GenerateQrCode::class, 'download'])->name('qr.maps.download');
+
+Route::get('/barcode', [GenerateBarcode::class, 'index'])->name('barcode.download');
+Route::get('/barcode-download', [GenerateBarcode::class, 'download'])->name('barcode.download');
