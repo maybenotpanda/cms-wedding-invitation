1e9d528267d9851b9e387613f89dc324b1c33ba7
maybenotpanda <maybenotpanda@gmail.com>
2025-09-16 02:48:59 +0700

    feat-messages: add new url api for list & add

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Invitation;
+use App\Http\Controllers\Message;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::get('v1/guest/{slug}', [Invitation::class, 'getBySlug']);
+
+/* MESSAGES */
+Route::post('v1/message/add', [Message::class, 'store']);
+Route::get('v1/message/list', [Message::class, 'index']);
