cbcaebfc4c2c7039426bbdd2606bbc0196714720
maybenotpanda <maybenotpanda@gmail.com>
2025-09-16 03:25:34 +0700

    feat-messages: changes controller in api

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Invitation;
-use App\Http\Controllers\Message;
+use App\Http\Controllers\MessageController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
