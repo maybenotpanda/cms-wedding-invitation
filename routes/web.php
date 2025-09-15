e6500deca52fe2fae76dde0689fa75635166f62e
maybenotpanda <maybenotpanda@gmail.com>
2025-09-16 03:30:25 +0700

    feat-route: add new route with controller for messages

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Home;
 use App\Http\Controllers\Invitation;
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\GenerateBarcode;
 use App\Http\Controllers\GenerateQrCode;
 
@@ -10,6 +11,9 @@ Route::get('/', [Home::class, 'index']);
 
 Route::get('/invitation', [Invitation::class, 'index']);
 Route::post('/invitation', [Invitation::class, 'store'])->name('invitation.store');
+
+/* MESSAGES */
+Route::get('/messages', [MessageController::class, 'index']);
 
 Route::get('/qr-maps', [GenerateQrCode::class, 'index']);
 Route::get('/qr-maps/download', [GenerateQrCode::class, 'download'])->name('qr.maps.download');
